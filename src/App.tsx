6af58b8a64ee9fea3ceee1374cd124cc795728a0
Rinat <rinat@kloop.kg>
2021-07-02 12:20:23 +0600

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import Form from '@rjsf/core';
 import './App.css';
 
+declare global {
+    interface Window { Android: any; }
+}
+window.Android = window.Android || {};
+// const evt = new Event('android_event', {'bubbles':true, 'cancelable':false});
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [schema, setSchema] = useState({});
+    const [uiSchema, setUiSchema] = useState({});
+    const [data, setData] = useState({});
+    const [exampleText, setExampleText] = useState("")
+
+    useEffect(() => {
+        //const evt = new Event('android_event', {'bubbles':true, 'cancelable':false});
+        // @ts-ignore
+        window.addEventListener('android_event', e => setExampleText(e.detail));
+        return () => {
+            //window.removeEventListener('android_event', e => {setExampleText(e + "Something is happening!")});
+            // @ts-ignore
+            window.removeEventListener('android_event', e => setExampleText(e.detail));
+        };
+    }, []);
+
+    // @ts-ignore
+    const handleChange = (v) => {
+        if ("Android" in window) {
+            window.Android.showToast(v);
+        }
+        // for (let prop in window)
+        //     console.log(prop);
+        // if ("Android" in window)
+        //     console.log(window.Android);
+    };
+
+    return (
+        <div>
+            <textarea value={exampleText}
+                      onChange={(e) => { handleChange(e.target.value); }} />
+            {/*<Form schema={schema}*/}
+            {/*      uiSchema={uiSchema}*/}
+            {/*      formData={data}*/}
+            {/*      onSubmit={handleSubmit}*/}
+            {/*/>*/}
+        </div>
+    );
 }
 
 export default App;
