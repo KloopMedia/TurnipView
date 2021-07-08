4dd8d6806c2973d71abaea3ddc8732ec50fec905
aTNa <atay.narynov@gmail.com>
2021-07-08 18:41:32 +0600

    Refactor schema

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,12 @@ import schemaUi from './ui.json'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 declare global {
-    interface Window { Android: any; }
+    interface Window {
+        Android: any;
+    }
 }
 window.Android = window.Android || {};
+
 // const evt = new Event('android_event', {'bubbles':true, 'cancelable':false});
 
 function App() {
@@ -17,27 +20,37 @@ function App() {
     const [uiSchema, setUiSchema] = useState({});
     const [data, setData] = useState({});
     const [fileData, setFileData] = useState({})
+    const [isComplete, setIsComplete] = useState(false)
 
     const widgets = {
         customfile: CustomFileWidget
     };
 
     useEffect(() => {
+        // // @ts-ignore
+        // window.addEventListener('android_json_event', (e: any) => {
+        //         console.log(JSON.stringify(e.detail))
+        //         setSchema(JSON.parse(e.detail))
+        //     }
+        // )
+        // // @ts-ignore
+        // window.addEventListener('android_ui_event', (e: any) => {
+        //         console.log(JSON.stringify(e.detail))
+        //         setUiSchema(JSON.parse(e.detail))
+        //     }
+        // )
         // @ts-ignore
-        window.addEventListener('android_json_event', (e: any) => {
+        window.addEventListener('android_schema_event', (e: any) => {
                 console.log(JSON.stringify(e.detail))
-                setSchema(JSON.parse(e.detail))
-            }
-        )
-        // @ts-ignore
-        window.addEventListener('android_ui_event', (e: any) => {
-                console.log(JSON.stringify(e.detail))
-                setUiSchema(JSON.parse(e.detail))
+                const stageData = JSON.parse(e.detail)
+                setSchema(stageData.jsonSchema)
+                setUiSchema(stageData.uiSchema)
+                setIsComplete(stageData.isComplete)
             }
         )
         // @ts-ignore
         window.addEventListener('android_data_event', (e: any) => {
-                console.log("JS FORMDATA",JSON.stringify(e.detail))
+                console.log("JS FORMDATA", JSON.stringify(e.detail))
                 setData(JSON.parse(e.detail))
             }
         )
